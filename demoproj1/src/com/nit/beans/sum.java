1752fd0bcc7d1bf0096e3b18b809ba2b2bf1095c
Hp <Hp@DESKTOP-J1PLGDT>
2024-07-14 23:13:43 +0530

    sum class to add the Date method

--- demoproj1/src/com/nit/beans/sum.java
+++ demoproj1/src/com/nit/beans/sum.java
@@ -1,9 +1,15 @@
 package com.nit.beans;
 
+import java.util.Date;
+
 public class sum {
 	public int sum(int a,int b)
 	{
 		return a+b;
 	}
 
+	  public Date getDate()
+	  {
+		  return new Date();
+	  }
 }
